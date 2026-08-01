444f0e1eab4f2935afa5e5a43fd11810b7c593f8
LordXn4 <marcosjardson@gmail.com>
2026-08-01 01:20:35 +0000

    fix minSdk 24 para speech_to_text

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.jabris_os"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
